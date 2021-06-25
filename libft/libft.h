9b4ff4f271f73566337a5de58c85318b7135f675
mikysett <mikysett@gmail.com>
2021-06-25 18:30:46 +0100

    ft_abs function added

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: msessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 14:33:59 by msessa            #+#    #+#             */
-/*   Updated: 2021/04/28 15:49:32 by msessa           ###   ########.fr       */
+/*   Updated: 2021/06/25 18:29:17 by msessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -66,5 +67,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),
 			void (*del)(void *));
+
+int		ft_abs(int nb);
 
 #endif
